1e8615e83b4b722deb57128836fa580012854cf2
ASHIQ MUHAMED KN <123819168+ashiqmhdkn@users.noreply.github.com>
2026-01-19 12:12:41 +0530

    profile update

--- src/profile.js
+++ src/profile.js
@@ -1,22 +1,22 @@
 import json from "./json";
 import { requireAuth } from "./auth";
 
-export  async function profileget(req, env) {
+export async function profileget(req, env) {
     const user = await requireAuth(req, env);
     if (!user) return json({ error: "Unauthorized" }, 401);
 
     const result = await env.cldb.prepare(
-        "SELECT user_id, email, name, role FROM users WHERE user_id = ?"
+        "SELECT * FROM users WHERE user_id = ?"
     ).bind(user.user_id).first();
 
     return json({ user: result });
 }
-export  async function profileput(req, env) {
+export async function profileput(req, env) {
     const user = await requireAuth(req, env);
     if (!user) return json({ error: "Unauthorized" }, 401);
-    const { name } = await req.json();
+    const { name, role, email, phone } = await req.json();
     await env.cldb.prepare(
-        "UPDATE users SET name = ? WHERE user_id = ?"
-    ).bind(name, user.user_id).run();
+        "UPDATE users SET name = ?, role = ?, email = ?, phone = ? WHERE user_id = ?"
+    ).bind(name, role, email, phone, user.user_id).run();
     return json({ success: true, message: "Profile updated successfully" });
 }
