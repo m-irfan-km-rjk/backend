e66f5049111a212343a8c70e52b1a0d9282db609
mirfankmrjk <mirfankm915@gmail.com>
2026-03-22 18:49:18 +0530

    Error fixed with login

--- src/users/signup.js
+++ src/users/signup.js
@@ -1,19 +1,31 @@
 import json from "../util/json";
 
 export default async function signup(req, env) {
-
     const body = await req.json();
-    const { email, password, name, role } = body;
+
+    if (!body.email || !body.password || !body.name) {
+        return json({ success: false, message: "Missing fields" }, 400);
+    }
+
+    const { email, password, name, role = "student" } = body;
 
     const id = crypto.randomUUID();
+
+    // 🔐 hash password
+    const hashedPassword = await hashPassword(password);
+
+    const image = `https://ui-avatars.com/api/?name=${name.replaceAll(" ", "+")}&background=random&color=random`;
 
     try {
         await env.cldb.prepare(
             "INSERT INTO users (user_id, email, password, name, role, image) VALUES (?, ?, ?, ?, ?, ?)"
-        ).bind(id, email, password, name, role, "https://ui-avatars.com/api/?name=" + name.replaceAll(" ", "+") + "&background=random&color=random").run();
+        ).bind(id, email, hashedPassword, name, role, image).run();
     } catch (error) {
         return json({ success: false, message: error.message }, 400);
     }
 
-    return json({ success: true, message: "User created successfully " + name }, 201);
+    return json({
+        success: true,
+        message: "User created successfully " + name
+    }, 201);
 }
